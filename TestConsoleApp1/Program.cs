dc36a8f4f69b5fd5c273da413c59469c9742c402
Michael Wöhr <woehr.mi@schmid-group.com>
2025-08-27 14:59:50 +0200

    Add test line to Program.cs for main branch
    Added a new line to output "This line was added on the main-branch!" to the console. This change is intended for testing purposes.

--- TestConsoleApp1/Program.cs
+++ TestConsoleApp1/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// add a test-line on the main branch:
+Console.WriteLine("This line was added on the main-branch!");
